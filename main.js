0fca80d46674c3ca25c25121c2618162a287aa85
Jacob Paris <jacob@jacobpariseau.com>
2019-05-28 19:49:18 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import jQuery from "./assets/js/jquery.min.js"
+window.$ = jQuery;
+
+import "./assets/js/jquery.scrolly.min.js"
+import "./assets/js/util.js"
+import "./assets/js/main.js"
+import './assets/sass/main.scss';
